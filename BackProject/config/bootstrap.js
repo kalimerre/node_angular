6c9d7f8eb7af14159ac86665f58c261fba73d113
lionel95200x <lionel95200@gmail.com>
2016-05-19 10:44:24 +0200

    git ignore modif

--- BackProject/config/bootstrap.js
+++ BackProject/config/bootstrap.js
@@ -11,6 +11,9 @@
 
 module.exports.bootstrap = function(cb) {
 
+    //sails.fs = require('fs');
+    //sails.chokidar = require('chokidar');
+
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
   cb();
