924d1f0508ef1ec34a6ec0d2174ac97bf2e079c4
lionel95200x <lionel95200@gmail.com>
2016-05-19 10:25:35 +0200

    Autorisation appel ajax entre front et middle avec exemple

--- FrontProject/app/scripts/controllers/about.js
+++ FrontProject/app/scripts/controllers/about.js
@@ -8,7 +8,13 @@
  * Controller of the frontProjectApp
  */
 angular.module('frontProjectApp')
-  .controller('AboutCtrl', function () {
+  .controller('AboutCtrl', function ($http,$scope) {
+
+  $http.get("http://localhost:1337/user").success(function(data){
+    $scope.users = data;
+  });
+
+
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
