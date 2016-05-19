58226658d0633dd4ec612ffa45eec5306afb00b9
lionel95200x <lionel95200@gmail.com>
2016-05-19 09:55:15 +0200

    Front without compass

---
+++ FrontProject/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontProjectApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the frontProjectApp
+ */
+angular.module('frontProjectApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ FrontProject/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontProjectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the frontProjectApp
+ */
+angular.module('frontProjectApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
